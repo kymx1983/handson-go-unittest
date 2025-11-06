1cc2cae63c73b4f72cfdf2fe49bb76cb088d1f08
kymx1983 <kymx1983@gmail.com>
2025-11-06 23:33:44 +0900

    初期設定: go.mod、main.go、および main_test.go ファイルを追加

---
+++ go.mod
@@ -0,0 +1,3 @@
+module handson-go-unittest
+
+go 1.22.6
---
+++ main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+// TestAdd は Add 関数の動作を確認するテストです。
+func TestAdd(t *testing.T) {
+	result := Add(2, 3)
+	expected := 5
+
+	if result != expected {
+		t.Errorf("Add(2, 3) = %d; want %d", result, expected)
+	}
+}
